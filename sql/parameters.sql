2186637797fe87961bee97b14812806cd4a0e96a
Matthias Bauw <matthias.bauw@gmail.com>
2013-01-27 21:05:01 +0100

    sql voor standaardparameters toegevoegd. Parameters AUDITIES_AANVRAAG_ACTIEF en AUDITIES_INSCHRIJVING_ACTIEF toegevoegd

---
+++ sql/parameters.sql
@@ -0,0 +1,3 @@
+INSERT into parameters (name,value) VALUES ('AUDITIES_AANVRAAG_ACTIEF','1');
+INSERT into parameters (name,value) VALUES ('AUDITIES_INSCHRIJVING_ACTIEF','1');
+insert into parameters (name,value) VALUES ('AUDITIES_ADMIN_MAIL','inschrijving@projectkoor.be');
