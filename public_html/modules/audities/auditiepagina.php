24d871a5fddda9a82a91e385c21852e3c3c4a761
Matthias Bauw <matthias.bauw@gmail.com>
2013-01-28 22:37:18 +0100

    2e schakelaar ingevoegd. Wanneer AUDITIES_INSCHRIJVING_ACTIEF gelijk is aan 0 kunnen enkel de definitief bevestigde kandidaten de auditiepagina nog raadplegen.

--- public_html/modules/audities/auditiepagina.php
+++ public_html/modules/audities/auditiepagina.php
@@ -4,38 +4,47 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/presentation/general/commonfunctio
 require_once $_SERVER['DOCUMENT_ROOT'].'/modules/audities/auditielogic.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/modules/audities/auditiekandidaat.php';
 
-if(!isset($_GET['key']))
+$kandidaat = checkKey($_GET['key']);
+
+if(getAuditieInschrijvingStatus() || inschrijvingBevestigd($kandidaat))
 {
-	ShowMessage('Geen toegang','om deze pagina te openen heb je een persoonlijke auditiesleutel nodig','/','Vraag je auditiepakket aan');
+    if(!isset($_GET['key']))
+    {
+            ShowMessage('Geen toegang','om deze pagina te openen heb je een persoonlijke auditiesleutel nodig','/','Vraag je auditiepakket aan');
+    }
+    else
+    {
+            ###ER is een sleutel opgegeven maar is die wel correcT?
+            
+
+            if($kandidaat instanceOf auditieKandidaat)
+            {
+                    $html = new HTMLPage('frontend');
+                    $html->LoadAddin('/modules/audities/auditiepagina.tpa');
+                    $html->setVariable('naam',$kandidaat->getNaam());
+                    $html->setVariable('voornaam',$kandidaat->getVoornaam());
+                    $html->setVariable('key',$kandidaat->getKey());
+
+                    ###Afhankelijk van de inschrijvingsstatus moet een andere melding weergegeven worden
+                    if(inschrijvingBevestigd($kandidaat))
+                    {
+                            $html->setVariable('welingeschreven',"ok");
+                    }
+                    else
+                    {
+                            $html->setVariable('nognietingeschreven',"ok");
+                    }
+
+                    $html->PrintHTML();
+            }
+            else
+            {
+                    showMessage('Foutieve sleutel','Sorry, het systeem kon uw sleutel niet herkennen. Neem contact op met webmaster@projectkoor.be voor meer info');
+            }
+    }
 }
 else
 {
-	###ER is een sleutel opgegeven maar is die wel correcT?
-	$kandidaat = checkKey($_GET['key']);
-	
-	if($kandidaat instanceOf auditieKandidaat)
-	{
-		$html = new HTMLPage('frontend');
-		$html->LoadAddin('/modules/audities/auditiepagina.tpa');
-		$html->setVariable('naam',$kandidaat->getNaam());
-		$html->setVariable('voornaam',$kandidaat->getVoornaam());
-		$html->setVariable('key',$kandidaat->getKey());
-		
-		###Afhankelijk van de inschrijvingsstatus moet een andere melding weergegeven worden
-		if(inschrijvingBevestigd($kandidaat))
-		{
-			$html->setVariable('welingeschreven',"ok");
-		}
-		else
-		{
-			$html->setVariable('nognietingeschreven',"ok");
-		}
-		
-		$html->PrintHTML();
-	}
-	else
-	{
-		showMessage('Foutieve sleutel','Sorry, het systeem kon uw sleutel niet herkennen. Neem contact op met webmaster@projectkoor.be voor meer info');
-	}
+    showMessage("Auditiepagina gedesactiveerd", "De deadline voor de inschrijvingen is verstreken. Mischien een volgende keer?");
 }
 ?>
