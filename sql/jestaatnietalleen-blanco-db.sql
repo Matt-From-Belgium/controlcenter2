4b14d3a402de403e88785ce34c70bdc78a69c4ff
Matthias Bauw <matthias.bauw@gmail.com>
2013-03-22 17:27:40 +0100

    installatiescript db aangepast

--- sql/jestaatnietalleen-blanco-db.sql
+++ sql/jestaatnietalleen-blanco-db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 08 feb 2013 om 11:45
+-- Genereertijd: 22 mrt 2013 om 17:26
 -- Serverversie: 5.5.30
 -- PHP-Versie: 5.4.6-1ubuntu1.1
 
@@ -34,14 +34,7 @@ CREATE TABLE IF NOT EXISTS `abonnees` (
   `confirmed` enum('Y','N') NOT NULL DEFAULT 'N',
   `secretkey` char(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `abonnees`
---
-
-INSERT INTO `abonnees` (`id`, `voornaam`, `familienaam`, `mailadres`, `confirmed`, `secretkey`) VALUES
-(7, 'Matthias', 'Bauw', 'matthiasba@projectkoor.be', 'Y', '7921c733b706068b87cec2d499200812');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,15 +46,7 @@ CREATE TABLE IF NOT EXISTS `abonnementen` (
   `id` int(2) NOT NULL AUTO_INCREMENT,
   `naam` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `abonnementen`
---
-
-INSERT INTO `abonnementen` (`id`, `naam`) VALUES
-(2, 'test'),
-(3, 'nieuwabo');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -74,16 +59,6 @@ CREATE TABLE IF NOT EXISTS `abonnementenlink` (
   `abonnee` int(5) NOT NULL,
   PRIMARY KEY (`abonnement`,`abonnee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden uitgevoerd voor tabel `abonnementenlink`
---
-
-INSERT INTO `abonnementenlink` (`abonnement`, `abonnee`) VALUES
-(2, 4),
-(2, 5),
-(2, 7),
-(3, 5);
 
 -- --------------------------------------------------------
 
@@ -378,14 +353,15 @@ CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(2) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `modules`
 --
 
 INSERT INTO `modules` (`id`, `name`) VALUES
-(01, 'Usermanagement');
+(01, 'Usermanagement'),
+(02, 'nieuwsbrief');
 
 -- --------------------------------------------------------
 
@@ -399,48 +375,6 @@ CREATE TABLE IF NOT EXISTS `nieuwsbriefabonnementen` (
   PRIMARY KEY (`nieuwsbrief`,`abonnement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Gegevens worden uitgevoerd voor tabel `nieuwsbriefabonnementen`
---
-
-INSERT INTO `nieuwsbriefabonnementen` (`nieuwsbrief`, `abonnement`) VALUES
-(1, 2),
-(2, 2),
-(3, 2),
-(4, 2),
-(5, 2),
-(6, 2),
-(7, 2),
-(8, 2),
-(9, 2),
-(10, 2),
-(11, 2),
-(12, 2),
-(13, 2),
-(14, 2),
-(15, 2),
-(16, 2),
-(17, 2),
-(18, 2),
-(19, 2),
-(20, 2),
-(21, 2),
-(22, 2),
-(23, 2),
-(24, 2),
-(25, 2),
-(26, 2),
-(27, 2),
-(28, 2),
-(29, 2),
-(30, 2),
-(31, 2),
-(32, 2),
-(33, 2),
-(34, 2),
-(35, 2),
-(36, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -452,51 +386,10 @@ CREATE TABLE IF NOT EXISTS `nieuwsbrieven` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `maand` int(2) NOT NULL,
   `jaar` int(4) NOT NULL,
-  `bestandsnaam` varchar(50) NOT NULL,
+  `titel` varchar(50) NOT NULL,
+  `verstuurd` enum('Y','N') DEFAULT 'N',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
-
---
--- Gegevens worden uitgevoerd voor tabel `nieuwsbrieven`
---
-
-INSERT INTO `nieuwsbrieven` (`id`, `timestamp`, `maand`, `jaar`, `bestandsnaam`) VALUES
-(1, '2013-02-06 20:02:48', 1, 2013, ''),
-(2, '2013-02-06 20:20:39', 1, 2013, ''),
-(3, '2013-02-06 20:21:55', 1, 2013, ''),
-(4, '2013-02-06 20:24:05', 1, 2013, ''),
-(5, '2013-02-06 20:27:06', 1, 2013, ''),
-(6, '2013-02-07 20:02:22', 2, 2013, ''),
-(7, '2013-02-07 20:02:46', 4, 2013, ''),
-(8, '2013-02-07 20:29:19', 5, 2013, ''),
-(9, '2013-02-07 20:30:11', 5, 2013, ''),
-(10, '2013-02-07 20:31:21', 5, 2013, ''),
-(11, '2013-02-07 20:31:43', 5, 2013, ''),
-(12, '2013-02-07 20:32:31', 5, 2013, ''),
-(13, '2013-02-07 20:34:49', 5, 2013, ''),
-(14, '2013-02-07 20:37:04', 5, 2013, ''),
-(15, '2013-02-07 20:41:15', 5, 2013, ''),
-(16, '2013-02-07 20:41:50', 5, 2013, ''),
-(17, '2013-02-07 20:43:52', 5, 2013, ''),
-(18, '2013-02-07 20:44:06', 5, 2013, ''),
-(19, '2013-02-07 20:44:27', 5, 2013, ''),
-(20, '2013-02-07 20:44:50', 5, 2013, ''),
-(21, '2013-02-07 20:44:58', 5, 2013, ''),
-(22, '2013-02-07 20:48:10', 5, 2013, ''),
-(23, '2013-02-07 20:48:27', 5, 2013, ''),
-(24, '2013-02-07 20:48:52', 5, 2013, ''),
-(25, '2013-02-07 20:53:20', 5, 2012, ''),
-(26, '2013-02-07 20:53:28', 5, 2012, ''),
-(27, '2013-02-07 20:53:50', 5, 2012, ''),
-(28, '2013-02-07 20:55:07', 5, 2012, ''),
-(29, '2013-02-07 20:55:30', 5, 2012, ''),
-(30, '2013-02-07 20:55:42', 5, 2012, ''),
-(31, '2013-02-07 20:56:10', 5, 2012, ''),
-(32, '2013-02-07 20:57:06', 5, 2012, ''),
-(33, '2013-02-07 20:57:20', 5, 2012, ''),
-(34, '2013-02-07 20:59:27', 5, 2102, ''),
-(35, '2013-02-07 20:59:47', 5, 2102, ''),
-(36, '2013-02-07 21:03:04', 5, 21013, '');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -518,11 +411,11 @@ CREATE TABLE IF NOT EXISTS `parameters` (
 --
 
 INSERT INTO `parameters` (`id`, `name`, `value`, `overridable`) VALUES
-(001, 'CORE_LANGUAGE', '2', b'0'),
+(001, 'CORE_LANGUAGE', '1', b'0'),
 (002, 'CORE_USER_SELF_ACTIVATION', '1', b'0'),
 (003, 'CORE_USER_ADMIN_ACTIVATION', '1', b'0'),
 (004, 'CORE_USER_EXT_USERGROUP', '001', b'0'),
-(005, 'CORE_USER_EXT_REGISTRATION', '1', b'0'),
+(005, 'CORE_USER_EXT_REGISTRATION', '0', b'0'),
 (006, 'CORE_NOACCESS_URL', '', b'0'),
 (007, 'CORE_SERVER_MAILADRESS', 'noreply@jestaatnietalleen.be', b'0');
 
@@ -537,7 +430,7 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `module` int(2) unsigned zerofill DEFAULT NULL,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `permissions`
@@ -548,7 +441,10 @@ INSERT INTO `permissions` (`id`, `module`, `name`) VALUES
 (00002, 01, 'add users'),
 (00003, 01, 'edit users'),
 (00004, NULL, 'interrupt service'),
-(00005, NULL, 'login during interruption');
+(00005, NULL, 'login during interruption'),
+(00006, 02, 'nieuwsbrieven versturen'),
+(00007, 02, 'beheerscherm openen'),
+(00008, 02, 'abonneedetails bekijken');
 
 -- --------------------------------------------------------
 
@@ -590,7 +486,9 @@ CREATE TABLE IF NOT EXISTS `usergroupmembers` (
 --
 
 INSERT INTO `usergroupmembers` (`user`, `usergroup`) VALUES
-(000001, 001);
+(000001, 001),
+(000001, 002),
+(000001, 003);
 
 -- --------------------------------------------------------
 
@@ -613,7 +511,11 @@ INSERT INTO `usergrouppermissions` (`usergroup`, `moduletask`) VALUES
 (001, 00002),
 (001, 00003),
 (001, 00004),
-(001, 00005);
+(001, 00005),
+(001, 00008),
+(002, 00006),
+(002, 00007),
+(003, 00007);
 
 -- --------------------------------------------------------
 
@@ -626,14 +528,16 @@ CREATE TABLE IF NOT EXISTS `usergroups` (
   `name` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `search_by_groupname` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `usergroups`
 --
 
 INSERT INTO `usergroups` (`id`, `name`) VALUES
-(001, 'administrators');
+(001, 'administrators'),
+(002, 'redactie'),
+(003, 'consulteren');
 
 -- --------------------------------------------------------
 
@@ -674,7 +578,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `passwordchangerequired`, `userconfirmation`, `adminconfirmation`, `realname`, `realfirstname`, `mailadress`, `website`, `country`) VALUES
-(000001, 'Admin', '71ea9c396816dc0db6571445abe2842f', '0', '1', '1', 'Bauw', 'Matthias', 'matthias.bauw@gmail.com', '', 'BE');
+(000001, 'Matt', '1d499a4fc2c8ce2b234233b08f103d3c', '0', '1', '1', 'Bauw', 'Matthias', 'matthias.bauw@gmail.com', '', 'BE');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
