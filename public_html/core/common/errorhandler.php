376f31134aa2568f89734c68f0e893aa0a9871b7
Matthias Bauw <matthias.bauw@gmail.com>
2013-05-12 11:27:13 +0200

    error handling toegevoegd aan auto_prepend

--- public_html/core/common/errorhandler.php
+++ public_html/core/common/errorhandler.php
@@ -4,6 +4,23 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/email/email.php';
 
 function CC_Exception_Handler(Exception $ex)
 {
+    ###We versturen het foutrapport
+    ###BUGFIX: moet voor het gooien van de exception komen als debug mode actief is anders wordt 
+    ###de mail niet verstuurd aangezien de exception alles stil legt.
+    $debugmailadress = getDebugMailadress();
+
+    $errorreport = new Email();
+    $errorreport->setTo($debugmailadress);
+    $errorreport->setMessageAddin('/core/presentation/general/addins/debugmail.tpa');
+    $errorreport->setSubject(LANG_ERROR_REPORT);
+    $errorreport->setVariable('message', $ex->getMessage());
+    $errorreport->setVariable('file', $ex->getFile());
+    $errorreport->setVariable('line', $ex->getLine());
+    $errorreport->setVariable('post', print_r($_POST,true));
+    $errorreport->setVariable('get', print_r($_GET,true));
+    $errorreport->Send();
+    
+    
      if(getDebugMode())
             {
                     ###We mogen de exception terug geven zoals normaal
@@ -14,19 +31,7 @@ function CC_Exception_Handler(Exception $ex)
                     echo 'serverfout';
             }
             
-                  ###We versturen het foutrapport
-                    $debugmailadress = getDebugMailadress();
 
-                    $errorreport = new Email();
-                    $errorreport->setTo($debugmailadress);
-                    $errorreport->setMessageAddin('/core/presentation/general/addins/debugmail.tpa');
-                    $errorreport->setSubject(LANG_ERROR_REPORT);
-                    $errorreport->setVariable('message', $ex->getMessage());
-                    $errorreport->setVariable('file', $ex->getFile());
-                    $errorreport->setVariable('line', $ex->getLine());
-                    $errorreport->setVariable('post', print_r($_POST,true));
-                    $errorreport->setVariable('get', print_r($_GET,true));
-                    $errorreport->Send();
 }
 
 set_exception_handler("CC_Exception_Handler");
