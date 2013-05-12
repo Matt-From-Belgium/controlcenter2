f915c96c01b05d8552a54cebd365f36de1e07315
Matthias Bauw <matthias.bauw@gmail.com>
2013-05-12 18:11:58 +0200

    versturen van het foutrapport gecentraliseerd

--- public_html/core/common/errorhandler.php
+++ public_html/core/common/errorhandler.php
@@ -4,6 +4,24 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/email/email.php';
 
 function CC_Exception_Handler(Exception $ex)
 {
+
+    CC_Send_Error_report($ex);
+    
+     if(getDebugMode())
+            {
+                    ###We mogen de exception terug geven zoals normaal
+                    throw $ex;
+            }
+            else
+            {            
+                    echo 'serverfout';
+            }
+            
+
+}
+
+function CC_Send_Error_report(Exception $ex)
+{
     ###We versturen het foutrapport
     ###BUGFIX: moet voor het gooien van de exception komen als debug mode actief is anders wordt 
     ###de mail niet verstuurd aangezien de exception alles stil legt.
@@ -20,19 +38,6 @@ function CC_Exception_Handler(Exception $ex)
     $errorreport->setVariable('post', print_r($_POST,true));
     $errorreport->setVariable('get', print_r($_GET,true));
     $errorreport->Send();
-    
-    
-     if(getDebugMode())
-            {
-                    ###We mogen de exception terug geven zoals normaal
-                    throw $ex;
-            }
-            else
-            {            
-                    echo 'serverfout';
-            }
-            
-
 }
 
 set_exception_handler("CC_Exception_Handler");
