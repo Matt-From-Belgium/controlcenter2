4fe4daae0831cb76b94faed54d23ab4f57c8c468
Matthias Bauw <matthias.bauw@gmail.com>
2013-05-12 10:24:40 +0200

    custom exception handler

---
+++ public_html/core/common/errorhandler.php
@@ -0,0 +1,33 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/core/logic/parameters.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/core/email/email.php';
+
+function CC_Exception_Handler(Exception $ex)
+{
+     if(getDebugMode())
+            {
+                    ###We mogen de exception terug geven zoals normaal
+                    throw $ex;
+            }
+            else
+            {            
+                    echo 'serverfout';
+            }
+            
+                  ###We versturen het foutrapport
+                    $debugmailadress = getDebugMailadress();
+
+                    $errorreport = new Email();
+                    $errorreport->setTo($debugmailadress);
+                    $errorreport->setMessageAddin('/core/presentation/general/addins/debugmail.tpa');
+                    $errorreport->setSubject(LANG_ERROR_REPORT);
+                    $errorreport->setVariable('message', $ex->getMessage());
+                    $errorreport->setVariable('file', $ex->getFile());
+                    $errorreport->setVariable('line', $ex->getLine());
+                    $errorreport->setVariable('post', print_r($_POST,true));
+                    $errorreport->setVariable('get', print_r($_GET,true));
+                    $errorreport->Send();
+}
+
+set_exception_handler("CC_Exception_Handler");
+?>
