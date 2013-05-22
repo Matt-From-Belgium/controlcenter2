0295974dd1d6ab6ec8001ad2e96d7a44f3f68e80
Matthias Bauw <matthias.bauw@gmail.com>
2013-05-22 21:49:39 +0200

    eerste stappen implementatie fotoupload

---
+++ public_html/modules/fotoalbum/presentation/editalbum.js
@@ -0,0 +1,28 @@
+function uploadPicture(formElement)
+{
+    //De functie wordt geactiveerd met het formulierelement als argument
+    //Alle formuliervelden worden automatisch klaargezet in het transactieobject
+    
+    var uploadTransaction = new ajaxTransaction(formElement);
+    
+    //Nu moeten we de voortgang tonen
+    createUploadMonitor(formElement);
+}
+
+function createUploadMonitor(formElement)
+{
+    var uploadsDiv = document.getElementById('uploads');
+ 
+    //We creëren de basisDiv waar we alles aan zullen hangen
+    var imageUploadMonitor = document.createElement('img');
+    
+    var file = document.getElementById('photopath').files[0];
+    
+    var filename = file.name;
+    var filesize = file.size;
+    
+    var title = document.createElement('div');
+    title.innerHTML = filename;
+    
+    
+}
--- public_html/modules/fotoalbum/presentation/editalbum.php
+++ public_html/modules/fotoalbum/presentation/editalbum.php
@@ -1,6 +1,9 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/modules/fotoalbum/logic/albumlogic.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/core/logic/usermanagement/userfunctions.php';
+
+checkPermission('fotoalbum', 'manage albums');
 
 if(isset($_GET['id']))
 {
