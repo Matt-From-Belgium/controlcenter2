dd3346d6814fe05fa4dee3fc0534771e40d0cc19
Matthias Bauw <matthias.bauw@gmail.com>
2013-11-07 22:06:13 +0100

    backup db

--- sql/jestaatnietalleen-blanco-db.sql
+++ sql/jestaatnietalleen-blanco-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
+-- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 22 mrt 2013 om 17:26
--- Serverversie: 5.5.30
--- PHP-Versie: 5.4.6-1ubuntu1.1
+-- Genereertijd: 07 nov 2013 om 22:04
+-- Serverversie: 5.5.34-0ubuntu0.13.04.1
+-- PHP-versie: 5.4.9-4ubuntu2.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -46,7 +46,15 @@ CREATE TABLE IF NOT EXISTS `abonnementen` (
   `id` int(2) NOT NULL AUTO_INCREMENT,
   `naam` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `abonnementen`
+--
+
+INSERT INTO `abonnementen` (`id`, `naam`) VALUES
+(1, 'Regio Veurne'),
+(2, 'Regio Diksmuide');
 
 -- --------------------------------------------------------
 
@@ -59,6 +67,18 @@ CREATE TABLE IF NOT EXISTS `abonnementenlink` (
   `abonnee` int(5) NOT NULL,
   PRIMARY KEY (`abonnement`,`abonnee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `albums`
+--
+
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -353,7 +373,7 @@ CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(2) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `modules`
@@ -361,7 +381,8 @@ CREATE TABLE IF NOT EXISTS `modules` (
 
 INSERT INTO `modules` (`id`, `name`) VALUES
 (01, 'Usermanagement'),
-(02, 'nieuwsbrief');
+(02, 'nieuwsbrief'),
+(03, 'fotoalbum');
 
 -- --------------------------------------------------------
 
@@ -400,11 +421,11 @@ CREATE TABLE IF NOT EXISTS `nieuwsbrieven` (
 CREATE TABLE IF NOT EXISTS `parameters` (
   `id` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `value` varchar(50) NOT NULL,
+  `value` varchar(500) NOT NULL,
   `overridable` bit(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `search_by_name` (`name`(20))
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `parameters`
@@ -417,7 +438,10 @@ INSERT INTO `parameters` (`id`, `name`, `value`, `overridable`) VALUES
 (004, 'CORE_USER_EXT_USERGROUP', '001', b'0'),
 (005, 'CORE_USER_EXT_REGISTRATION', '0', b'0'),
 (006, 'CORE_NOACCESS_URL', '', b'0'),
-(007, 'CORE_SERVER_MAILADRESS', 'noreply@jestaatnietalleen.be', b'0');
+(007, 'CORE_SERVER_MAILADRESS', 'noreply@jestaatnietalleen.be', b'0'),
+(008, 'NIEUWSBRIEF_PROMOTEXT', 'Schrijf je in voor de nieuwsbrief en kom alles te over je rechten en plichten als KMO-werknemer. Bovendien ontvang je alle gegevens over de acties die we organiseren in onze regio.', b'0'),
+(009, 'CORE_DEBUG_MAIL', 'matthiasba@Linux.be', b'0'),
+(010, 'CORE_DEBUG_MODE', '0', b'0');
 
 -- --------------------------------------------------------
 
@@ -430,7 +454,7 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `module` int(2) unsigned zerofill DEFAULT NULL,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `permissions`
@@ -444,7 +468,22 @@ INSERT INTO `permissions` (`id`, `module`, `name`) VALUES
 (00005, NULL, 'login during interruption'),
 (00006, 02, 'nieuwsbrieven versturen'),
 (00007, 02, 'beheerscherm openen'),
-(00008, 02, 'abonneedetails bekijken');
+(00008, 02, 'abonneedetails bekijken'),
+(00009, 03, 'manage albums');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `photos`
+--
+
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `extension` varchar(4) NOT NULL,
+  `album` int(3) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -465,9 +504,9 @@ CREATE TABLE IF NOT EXISTS `templatealiases` (
 --
 
 INSERT INTO `templatealiases` (`id`, `name`, `directory`) VALUES
-(01, 'frontend', 'alpha3'),
-(02, 'backend', 'alpha3'),
-(03, 'mail', 'mail');
+(01, 'frontend', 'jestaatnietalleen'),
+(02, 'backend', 'jestaatnietalleen'),
+(03, 'mail', 'jestaatnietalleen-mail');
 
 -- --------------------------------------------------------
 
@@ -512,7 +551,10 @@ INSERT INTO `usergrouppermissions` (`usergroup`, `moduletask`) VALUES
 (001, 00003),
 (001, 00004),
 (001, 00005),
+(001, 00006),
+(001, 00007),
 (001, 00008),
+(001, 00009),
 (002, 00006),
 (002, 00007),
 (003, 00007);
