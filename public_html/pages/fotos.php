2d5a01686e89dcd8191bbfa68d8924bb7f3a1208
Matthias Bauw <matthias.bauw@gmail.com>
2013-11-11 14:04:12 +0100

    aanpassingen mobiele versie

---
+++ public_html/pages/fotos.php
@@ -0,0 +1,17 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/modules/nieuwsbrief/logic/nieuwsbrieflogic.php';
+
+###Promotieberichtje in het nieuwsbriefformulier
+$promotext = getNewsPromoText();
+
+
+$html = new htmlpage('backend');
+$html->LoadAddin('/pages/addins/fototest.tpa');
+$html->setVariable('promotext', $promotext);
+$html->PrintHTML();
+
+
+
+?>
+
