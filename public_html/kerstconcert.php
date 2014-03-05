abf68556b4d2f0db9194a44ef2b6ab5020cd4479
Matthias <matthias.bauw@gmail.com>
2014-03-05 20:04:18 +0100

    aanpassingen scripts laden kerstconcert

--- public_html/kerstconcert.php
+++ public_html/kerstconcert.php
@@ -3,5 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php'
 
 $html = new htmlpage('frontend');
 $html->LoadAddin('/addins/kerstconcert.tpa');
+$html->loadScript('/modules/fotoalbum/presentation/showalbum.js');
+
 $html->PrintHTML();
 ?>
