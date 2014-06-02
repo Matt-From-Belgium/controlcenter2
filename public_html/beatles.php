0625ab39cf8d0b58f755cc985d90ce33962da918
Matthias <matthias.bauw@gmail.com>
2014-06-02 20:57:15 +0200

    kleine aanpassingen FB Beatles + tekst bevestigingsmails tickets

--- public_html/beatles.php
+++ public_html/beatles.php
@@ -43,7 +43,7 @@ $html->addCustomMeta('og:type', $fbNameSpace.':concert');
 $html->addCustomMeta('fb:app_id', $fbAppId);
 $html->addCustomMeta('og:url', 'http://www.projectkoorchantage.be/beatles.php');
 $html->addCustomMeta('og:title','Projectkoor CHANTage brengt Beatles-concert');
-$html->addCustomMeta('og:image', 'http://www.projectkoorchantage.be/images/beatles/beatlesFB.png');
+$html->addCustomMeta('og:image', 'http://www.projectkoorchantage.be/images/beatles/beatlesFB2.png');
 $html->addCustomMeta('og:description',"In 1964 beleefden 'The Beatles' hun grote doorbraak. Ze werden de grondleggers van de hedendaagse popmuziek. En toch... zo heeft u hen nog nooit gehoord. Projectkoor CHANTage brengt u een mix van hun meest gekende nummers.");
 }
 else
@@ -55,7 +55,7 @@ else
     $html->addCustomMeta('og:locale', 'nl_NL');
     $html->addCustomMeta('og:url', "http://www.projectkoorchantage.be/beatles.php?id=".$selectedsong->getID());
     $html->addCustomMeta('og:title',"Mijn favoriete Beatles-hit: ".$selectedsong->getTitle());
-    $html->addCustomMeta('og:image', 'http://www.projectkoorchantage.be/images/beatles/beatlesFB.png');
+    $html->addCustomMeta('og:image', 'http://www.projectkoorchantage.be/images/beatles/beatlesFB2.png');
     $html->addCustomMeta('og:description',"Wat is jouw favoriete Beatles-hit? Stem hier!");
 
 }
