8f74b86055d0630309ab337cb86aaafb0659ca71
Matthias <matthias.bauw@gmail.com>
2014-07-05 10:34:20 +0200

    mobiele detectie verschoven naar serverside

--- public_html/core/templatesystem/templatelogic.php
+++ public_html/core/templatesystem/templatelogic.php
@@ -1,10 +1,27 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/core/templatesystem/templatedata.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/core/templatesystem/templatefile.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/core/templatesystem/mobiledetect/mobiledetect.php";
 
 function AliasGetlinkeddir($alias)
 {
-	return DataAccess_AliasGetlinkeddir($alias);
+        ###Templateystem R3: we moeten detecteren welk platform gebruikt wordt
+        $platform = 'pc';
+        
+        $detection = new Mobile_Detect();
+        if($detection->isTablet())
+        {
+            $platform = 'tablet';
+        }
+        else
+        {
+            if($detection->isMobile())
+            {
+                $platform = 'phone';
+            }
+        }
+    
+	return DataAccess_AliasGetlinkeddir($alias,$platform);
 }
 
 function GetTemplatehtml($directory)
