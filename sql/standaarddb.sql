9982273981bcef73f43690040faf69321990ba3b
Matthias <matthias.bauw@gmail.com>
2014-07-29 17:09:26 +0200

    standaarddb was verloren gegaan

--- sql/standaarddb.sql
+++ sql/standaarddb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6deb1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2014 at 08:32 AM
--- Server version: 5.5.37-0ubuntu0.13.10.1
--- PHP Version: 5.5.3-1ubuntu2.5
+-- Generation Time: Jul 29, 2014 at 05:08 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,32 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `permissions`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `facebookid` varchar(20) DEFAULT NULL,
-  `password` varchar(32) NOT NULL,
-  `passwordchangerequired` enum('1','0') NOT NULL DEFAULT '0',
-  `userconfirmation` enum('1','0') NOT NULL DEFAULT '0',
-  `adminconfirmation` enum('1','0') NOT NULL DEFAULT '0',
-  `realname` varchar(30) NOT NULL,
-  `realfirstname` varchar(30) NOT NULL,
-  `mailadress` varchar(50) NOT NULL,
-  `website` varchar(50) DEFAULT NULL,
-  `country` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `search_by_username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `module` int(2) unsigned zerofill DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `permissions`
 --
 
-INSERT INTO `users` (`id`, `username`, `facebookid`, `password`, `passwordchangerequired`, `userconfirmation`, `adminconfirmation`, `realname`, `realfirstname`, `mailadress`, `website`, `country`) VALUES
-(000001, 'Admin', '', '71ea9c396816dc0db6571445abe2842f', '0', '1', '1', 'Bauw', 'Matthias', 'matthias@ittakestwo.be', '', 'BE');
+INSERT INTO `permissions` (`id`, `module`, `name`) VALUES
+(00001, 01, 'manage usergroups'),
+(00002, 01, 'add users'),
+(00003, 01, 'edit users'),
+(00004, NULL, 'interrupt service'),
+(00005, NULL, 'login during interruption');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
