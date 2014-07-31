1658251de111dabe990811ff84eec1d1daa2eaec
Matt <matthias.bauw@gmail.com>
2014-07-31 20:43:05 +0200

    implementatie van cookies notificatie in het template

--- public_html/index.php
+++ public_html/index.php
@@ -3,5 +3,6 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php'
 
 $html = new htmlpage('backend');
 $html->LoadAddin('/pages/addins/index.tpa');
+$html->loadCSS('/templates/jestaatnietalleen/css/index.css');
 $html->PrintHTML(); 
 ?>
