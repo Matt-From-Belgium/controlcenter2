447aaeb03c9f75aada8979d3f6c2299e4ec94272
Matthias <matthias.bauw@gmail.com>
2014-08-15 11:03:48 +0200

    nog bugfixes nodig

--- public_html/core/dataconnection/components/mysql/component.php
+++ public_html/core/dataconnection/components/mysql/component.php
@@ -6,12 +6,13 @@
  * and open the template in the editor.
  */
 
-class DataConnectionNew
+
+class DataConnection
 {
     Private $parameters;
     Private $mysqli;
     Private $activeQuery;
-    Private $queryResult;
+    Private $queryResult=false;
     Private $attributes=array();
     Private $queryText;
     
@@ -54,7 +55,17 @@ class DataConnectionNew
             #$matches[2] = het datatype zonderhaakjes
             #$matches[3] = de naam van het attribuut
             #Enkel 2 en 3 zijn interessant de rest is om het patroon te doen werken
+            
+            ###Oude code definieert geen datatype => we nemen string als standaard
+            if(empty($matches [2]))
+            {
+                $matches[2]='s';
+            }
+            
             $this->attributes[strtolower($matches[3])]['datatype']=strtolower($matches[2]);
+            
+            ###We geven ook een lege value aan ieder veld, deze wordt later ingevuld
+            $this->attributes[strtolower($matches[3])]['value']=null;
             
             $replacement = '?';
             return $replacement;
@@ -66,14 +77,10 @@ class DataConnectionNew
         $this->queryText = $query;
         
         ##ons formaat van query omzetten naar formaat met ? dat door mysqli begrepen wordt
-        $pattern = "/(?i)@(\((i|d|s|b)\))?([a-z0-9]*)/";
+        ##Ook de haakjes rond de attributen moeten weg
+        $pattern = "/(?i)'?@(\((i|d|s|b)\))?([a-z0-9]*)'?/";
         
-        //preg_match_all($pattern, $query, $attributes,PREG_SET_ORDER);
         $query=preg_replace_callback($pattern,array($this,'replaceAttribute'), $query);
-        
-        
-        print_r($this->attributes);
-        echo $query;
         
         
         ###We geven de query door aan MySQLi
@@ -82,28 +89,52 @@ class DataConnectionNew
         ###Als hierna $this->activeQuery geen waarde heeft is er iets fout gelopen
         if(!$this->activeQuery)
         {
-            throw new Exception($this->mysqli->error, $this->mysqli->errno);
+            throw new Exception('Query syntax is not correct. MySQL reported this error: '.$this->mysqli->error.' while preparing this statement: '.$this->queryText, $this->mysqli->errno);
         }
     }
     
     public function ExecuteQuery()
-    {
-        ###We voeren de query uit
-        $confirmation=$this->activeQuery->execute();
-        
-        if($confirmation)
+    {    
+        if($this->activeQuery)
         {
-            ###Query is geslaagd
-            ###We halen het resultaat op
-            $this->queryResult = $this->activeQuery->get_result();
-        }
-        else
-        {
-            ###Query is niet geslaagd
-            throw new Exception($this->mysqli->error, $this->mysqli->errno);
-        }
+            $typestring = null;
+            $valuearray = array();
+
+            foreach ($this->attributes as &$value)
+            {
+                $typestring = $typestring.$value['datatype'];
+
+                $valuearray[]=&$value['value'];
+            }
+
+            $mergedarray[] = $typestring;
+            $mergedarray = array_merge($mergedarray,$valuearray);
+
+            ###Hier roepen we eigenlijk bind_param aan maar met de $mergedarray als argumenten
+            if(count($this->attributes)>0)
+            {
+                call_user_func_array(array($this->activeQuery,'bind_param'), $mergedarray);
+            }
+
+            ###We voeren de query uit
+            $confirmation=$this->activeQuery->execute();
+
+            if($confirmation)
+            {
+                ###Query is geslaagd
+                ###We halen het resultaat op
+                $this->queryResult = $this->activeQuery->get_result();
+            }
+            else
+            {
+                ###Query is niet geslaagd
+                throw new Exception('There was an error while executing the query '. $this->queryText . 'with attributearray' . print_r($this->attributes).' Mysql reported this error '.$this->mysqli->error, $this->mysqli->errno);
+            }
         
-        
+        }
+        else {
+              throw new Exception("Impossible to execute a query if you haven't set one");
+        }
     }
     
     public function setAttribute($attributename,$value)
@@ -113,7 +144,7 @@ class DataConnectionNew
         if($this->attributes[$attributename])
         {
             ###Het attribuut bestaat in de query
-            echo 'ok';
+            $this->attributes[$attributename]['value']=&$value;
         }
         else
         {
@@ -125,12 +156,45 @@ class DataConnectionNew
     
     public function GetResultArray()
     {
-        return $this->queryResult->fetch_array();
+        if($this->queryResult)
+        {
+        return $this->queryResult->fetch_all(MYSQLI_BOTH);
+        }
+        else
+        {
+            throw new Exception('you must first execute the query before getting the results');
+        }
+    }
+    
+    public function getRowAsObject()
+    {
+        ###deze functie geeft één rij terug als een object
+        if($this->queryResult)
+        {
+                
+                return $this->queryResult->fetch_object();
+        }
+        else
+        {
+            throw new Exception('you must first execute the query before getting the results');
+        }
+
     }
     
     public function getScalar()
     {
-        
+        ###bij scalar willen we enkel het eerste veld van de eerste rij krijgen.
+        ###Eerst controleren we of het wel degelijk om een scalar gaat
+        if(($this->queryResult->num_rows==1) && ($this->queryResult->field_count==1))
+        {
+            ###Het gaat effectief om een scalar, we geven de waarde terug.
+            $row = $this->queryResult->fetch_row();
+            return $row[0];
+        }
+        else
+        {
+            throw new Exception('You tried getting the result with getScalar, but the result is not a scalar');
+        }
     }
     
     public function GetNumRows()
@@ -144,11 +208,14 @@ class DataConnectionNew
     }
 }
 
-//DEBUG
-$debug = new DataConnectionNew;
-$debug->setQuery('SELECT * from users WHERE users.id=@(i)id and users.username=@(s)username');
-$debug->setAttribute('ide', 1);
-//$debug->ExecuteQuery();
-//echo $debug->GetNumRows();
-//$result=$debug->GetResultArray();
-//print_r($result);
+/*//DEBUG
+$debug = new DataConnection;
+$debug->setQuery("SELECT parameters.id,parameters.name,parameters.value,parameters.overridable FROM parameters WHERE parameters.name='@searchstring'");
+$debug->setAttribute('searchstring', 'CORE_SSL_ENABLED');
+$debug->ExecuteQuery();
+
+$result=$debug->GetResultArray();
+
+print_r($result);*/
+
+ 
