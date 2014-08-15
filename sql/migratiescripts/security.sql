376548d85788b2072c5a756bafb168e5e16cd9d8
Matthias <matthias.bauw@gmail.com>
2014-08-15 14:02:42 +0200

    bugfix in migratiescript

--- sql/migratiescripts/security.sql
+++ sql/migratiescripts/security.sql
@@ -1,11 +1,11 @@
-INSERT INTO `controlcenter`.`parameters` (`name` ,`value` ,`overridable`) VALUES ('CORE_SSL_ENABLED', '0', '0');
+INSERT INTO parameters (name ,value ,overridable) VALUES ('CORE_SSL_ENABLED', '0', '0');
 
 ALTER TABLE `users` CHANGE `password` `password` VARCHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
 ALTER TABLE  `users` ADD  `salt` VARCHAR( 128 ) NOT NULL AFTER  `password` ;
 
 CREATE TABLE IF NOT EXISTS `ajaxwhitelist` (
   `id` int(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `filename` varchar(500) NOT NULL,
+  `file` varchar(500) NOT NULL,
   `function` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
