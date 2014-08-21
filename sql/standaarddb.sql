1063ded3a4bc4fa6ce068744819666cfbd8d49e2
Matthias <matthias.bauw@gmail.com>
2014-08-21 21:27:28 +0200

    nieuwe standaard SQL

--- sql/standaarddb.sql
+++ sql/standaarddb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 15, 2014 at 05:17 PM
+-- Generation Time: Aug 21, 2014 at 09:26 PM
 -- Server version: 5.5.38-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.3
 
@@ -19,6 +19,252 @@ SET time_zone = "+00:00";
 --
 -- Database: `controlcenter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ajaxwhitelist`
+--
+
+CREATE TABLE IF NOT EXISTS `ajaxwhitelist` (
+  `id` int(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `file` varchar(500) NOT NULL,
+  `function` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `file` (`file`),
+  KEY `function` (`function`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `ajaxwhitelist`
+--
+
+INSERT INTO `ajaxwhitelist` (`id`, `file`, `function`) VALUES
+(000001, '/core/logic/usermanagement/fbLoginAjax.php', 'checkFBAccount'),
+(000002, '/core/templatesystem/templatelogic.php', 'setCookiesOk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
+CREATE TABLE IF NOT EXISTS `languages` (
+  `id` int(2) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `search_by_name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `languages`
+--
+
+INSERT INTO `languages` (`id`, `name`) VALUES
+(01, 'nederlands'),
+(02, 'english');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`,`time`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+  `id` int(2) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `name`) VALUES
+(01, 'Usermanagement');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parameters`
+--
+
+CREATE TABLE IF NOT EXISTS `parameters` (
+  `id` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(500) NOT NULL,
+  `overridable` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `search_by_name` (`name`(20)),
+  KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+
+--
+-- Dumping data for table `parameters`
+--
+
+INSERT INTO `parameters` (`id`, `name`, `value`, `overridable`) VALUES
+(001, 'CORE_LANGUAGE', '1', b'1'),
+(002, 'CORE_USER_SELF_ACTIVATION', '1', b'0'),
+(003, 'CORE_USER_ADMIN_ACTIVATION', '0', b'0'),
+(004, 'CORE_USER_EXT_USERGROUP', '001', b'0'),
+(005, 'CORE_USER_EXT_REGISTRATION', '0', b'0'),
+(006, 'CORE_NOACCESS_URL', '', b'0'),
+(007, 'CORE_SERVER_MAILADRESS', 'noreply@dragoneyehosting.be', b'0'),
+(008, 'CORE_DEBUG_MODE', '0', b'0'),
+(009, 'CORE_DEBUG_MAIL', 'matthiasba@linux.be', b'0'),
+(010, 'CORE_FB_LOGIN_ENABLED', '0', b'0'),
+(011, 'CORE_FB_APPID', '', b'0'),
+(012, 'CORE_FB_SAPPID', '', b'0'),
+(013, 'CORE_FB_SCOPE', 'email', b'0'),
+(014, 'CORE_SITE_NAME', 'CONTROLCENTER SERVER', b'0'),
+(015, 'CORE_FB_APP_NAMESPACE', '', b'0'),
+(016, 'SITE_META_TITLE', 'CONTROLCENTER SERVER', b'0'),
+(017, 'SITE_META_DESCRIPTION', 'Controlcenter testserver', b'0'),
+(022, 'CORE_SSL_ENABLED', '0', b'0'),
+(018, 'SITE_META_IMAGE', '', b'0'),
+(019, 'SITE_META_URL', '', b'0'),
+(020, 'CORE_RECAPTCHA_PUBLIC', '6LdR-eYSAAAAAEboUatksFIHeb6m4CvmTT1-7_5p ', b'0'),
+(021, 'CORE_RECAPTCHA_PRIVATE', '6LdR-eYSAAAAAM2V3SNVOLmiFEQJOz6TrgWBNfu3 ', b'0'),
+(023, 'CORE_MAINTENANCE_MODE', '1', b'0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `module` int(2) unsigned zerofill DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `module`, `name`) VALUES
+(00001, 01, 'manage usergroups'),
+(00002, 01, 'add users'),
+(00003, 01, 'edit users'),
+(00004, NULL, 'interrupt service'),
+(00005, NULL, 'login during interruption');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `templatealiases`
+--
+
+CREATE TABLE IF NOT EXISTS `templatealiases` (
+  `id` int(2) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `pc_directory` varchar(30) NOT NULL,
+  `phone_directory` varchar(30) DEFAULT NULL,
+  `tablet_directory` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `search_by_name` (`name`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `templatealiases`
+--
+
+INSERT INTO `templatealiases` (`id`, `name`, `pc_directory`, `phone_directory`, `tablet_directory`) VALUES
+(01, 'frontend', 'alpha3', NULL, NULL),
+(02, 'backend', 'alpha3', NULL, NULL),
+(03, 'mail', 'mail', NULL, NULL),
+(04, 'maintenance', 'maintenance', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroupmembers`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroupmembers` (
+  `user` int(6) unsigned zerofill NOT NULL,
+  `usergroup` int(3) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`user`,`usergroup`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `usergroupmembers`
+--
+
+INSERT INTO `usergroupmembers` (`user`, `usergroup`) VALUES
+(000001, 001);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergrouppermissions`
+--
+
+CREATE TABLE IF NOT EXISTS `usergrouppermissions` (
+  `usergroup` int(3) unsigned zerofill NOT NULL,
+  `moduletask` int(5) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`usergroup`,`moduletask`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `usergrouppermissions`
+--
+
+INSERT INTO `usergrouppermissions` (`usergroup`, `moduletask`) VALUES
+(001, 00001),
+(001, 00002),
+(001, 00003),
+(001, 00004),
+(001, 00005);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroups`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroups` (
+  `id` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `search_by_groupname` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `usergroups`
+--
+
+INSERT INTO `usergroups` (`id`, `name`) VALUES
+(001, 'administrators');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userpermissions`
+--
+
+CREATE TABLE IF NOT EXISTS `userpermissions` (
+  `user` int(6) unsigned zerofill NOT NULL,
+  `moduletask` int(5) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`user`,`moduletask`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,8 +287,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `website` varchar(50) DEFAULT NULL,
   `country` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `search_by_username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=75 ;
+  UNIQUE KEY `search_by_username` (`username`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=76 ;
 
 --
 -- Dumping data for table `users`
