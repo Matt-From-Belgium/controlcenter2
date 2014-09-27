7419cdc1ca1e14f8f0982e8687bd2c67e1ee3b6d
Matthias <matthias.bauw@gmail.com>
2014-09-27 15:43:33 +0200

    update nalv start beatlesconcerten

--- public_html/core/pathtoconfig.php
+++ public_html/core/pathtoconfig.php
@@ -1,5 +1,5 @@
 <?php
 #Pad naar de configuratiefile
 #Plaats deze op een niet publiek toegankelijke locatie;
-$pathtoconfig = "/wwwroot/controlcenter/cc2config";
+$pathtoconfig = "/wwwroot/chantage/cc2config";
 ?>
