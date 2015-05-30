6f641ab8116d678c74ae99079662a0e393ac3fd0
Matthias Bauw <matthias.bauw@gmail.com>
2015-05-30 11:25:49 +0200

    aanpassingen nieuwsbrieven pagina

--- public_html/pages/nieuwsbrieven.php
+++ public_html/pages/nieuwsbrieven.php
@@ -9,6 +9,15 @@ $promotext = getNewsPromoText();
 $html = new htmlpage('frontend');
 $html->LoadAddin('/pages/addins/nieuwsbrieven.tpa');
 $html->loadScript('/modules/nieuwsbrief/scripts/facebook.js');
+$html->loadCSS('/pages/css/nieuwsbrieven-desktop.css','/pages/css/nieuwsbrieven-mobile.css');
+$html->addCustomMeta('description', 'Werk je in een Kleine of Middelgrote onderneming en wil je op de hoogste blijven van belangrijke wijzigingen? Abonneer je dan hier op de nieuwsbrief.');
+$html->addCustomMeta('og:type', 'website');
+$html->addCustomMeta('og:url', 'http://www.jestaatnietalleen.be/pages/nieuwsbrieven.php');
+$html->addCustomMeta('og:title', 'Nieuwsbrief voor KMO-werknemers');
+$html->addCustomMeta('og:description', 'Werk je in een Kleine of Middelgrote onderneming en wil je op de hoogste blijven van belangrijke wijzigingen? Abonneer je dan hier op de nieuwsbrief.');
+$html->addCustomMeta('og:image', 'http://www.jestaatnietalleen.be/images/facebookmetalogo.png');
+  
+
 $html->setVariable('promotext', $promotext);
 
 //Facebooklink enkel tonen wanneer integratie actief is
