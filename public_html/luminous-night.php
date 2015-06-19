2a9ac46469f659a21cff4c6ef0bbf846598b657d
Matthias Bauw <matthias.bauw@gmail.com>
2015-06-19 16:27:35 +0200

    tekstzetting luminous

--- public_html/luminous-night.php
+++ public_html/luminous-night.php
@@ -4,6 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php'
 $html = new htmlpage('minimalistic');
 $html->LoadAddin('/addins/luminous-night.tpa');
 $html->loadCSS('/modules/fotoalbum/presentation/css/showphoto.css');
+$html->loadCSS('/extracss/luminous.css');
 $html->loadScript('/modules/fotoalbum/presentation/showalbum.js');
 
 $html->PrintHTML();
