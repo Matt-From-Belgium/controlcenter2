ce19d284b0c524166c534416bf8e61f8a9eb590c
Matthias Bauw <matthias.bauw@gmail.com>
2015-06-22 18:10:20 +0200

    bugfix functionaliteit luminous

--- public_html/luminous-night.php
+++ public_html/luminous-night.php
@@ -3,6 +3,17 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php'
 
 $html = new htmlpage('minimalistic');
 $html->LoadAddin('/addins/luminous-night.tpa');
+
+$fbAppId = getFacebookAppID();
+$fbNameSpace = getFacebookNameSpace();
+$html->addCustomMeta('og:type', $fbNameSpace.':concert');
+$html->addCustomMeta('fb:app_id', $fbAppId);
+$html->addCustomMeta('og:url', 'http://www.projectkoorchantage.be/luminous-night.php');
+$html->addCustomMeta('og:title','Luminous Night of The Soul');
+$html->addCustomMeta('og:image', 'http://www.projectkoorchantage.be/images/luminous/luminousfb.png');
+$html->addCustomMeta('og:description',"Projectoor CHANTage neemt u mee van donker naar licht. Het filmisch aandoend programma bevat werk van gevestigde waarden uit de klassieke muziek. Tijdens dit sfeervolle concert zullen wij de herst even verjagen...");
+
+
 $html->loadCSS('/modules/fotoalbum/presentation/css/showphoto.css');
 $html->loadCSS('/extracss/luminous.css');
 $html->loadScript('/modules/fotoalbum/presentation/showalbum.js');
