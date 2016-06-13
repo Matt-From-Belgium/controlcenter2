679d3d910cc12a17e32462ee13fd713247505d3d
Matt-From-Belgium <matthias.bauw@gmail.com>
2016-06-13 21:02:23 +0200

    site terug op orde gezet

--- public_html/audities.php
+++ public_html/audities.php
@@ -2,6 +2,8 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/core/templatesystem/templatesystem.php';
 
 $html = new htmlpage('frontend');
-$html->LoadAddin('/addins/audities.tpa');
+/*$html->LoadAddin('/addins/audities.tpa');*/
+$html->loadAddin('/modules/audities/geenaudities.tpa');
+$html->setVariable('mailadres', 'inschrijvingen@projectkoorchantage.be');
 $html->PrintHTML();
 ?>
